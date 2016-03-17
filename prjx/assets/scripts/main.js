c316af4b5b7d53aed80dedc6d82b574e5e7f6421
Rodan Luzuriaga <rodanaluzuriaga@gmail.com>
2016-03-17 17:58:35 +0800

    .

--- prjx/assets/scripts/main.js
+++ prjx/assets/scripts/main.js
@@ -4,7 +4,26 @@ var Main = (function () {
 	// object
 	return {
 		init: function () {
-			
+			$(document).ready(function() {
+			  // The slider being synced must be initialized first
+			  $('#carousel').flexslider({
+			    animation: "slide",
+			    controlNav: false,
+			    animationLoop: false,
+			    slideshow: false,
+			    itemWidth: 210,
+			    itemMargin: 5,
+			    asNavFor: '#slider'
+			  });
+
+			  $('#slider').flexslider({
+			    animation: "slide",
+			    controlNav: false,
+			    animationLoop: false,
+			    slideshow: false,
+			    sync: "#carousel"
+			  });
+			});
 		},
 
 		//--------
