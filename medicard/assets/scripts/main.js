30f6a328b2c59340a7cf1c7f720ffba92292db1a
Rodan Luzuriaga <rodanaluzuriaga@gmail.com>
2016-08-16 16:23:55 +0800

    ..

--- medicard/assets/scripts/main.js
+++ medicard/assets/scripts/main.js
@@ -16,6 +16,26 @@ var Main = (function () {
 				$('.mobile-nav').removeClass('move');
 			});
 
+			Main.scroll();
+
+		},
+
+		scroll: function() {
+			var constant = 200; // galing to sa Purple bar sa taas at Blue bar sa baba.
+													// Plus yung margin-top(na 20px) ng Blue bar.
+													// Hindi kase ma-access yung ::before tsaka ::after na
+													// pseudo selector sa jquery.
+													// nagdagdag din ako 25px pa na allowance.
+
+			var heads = $('.heads').outerHeight();
+			var tails = $('.tails').outerHeight();
+			var timeline = $('.timeline').outerHeight();
+
+			$(window).on('load scroll', function() {
+				if ( $(this).width() > 991 ) {
+					$('.scroll').height( $(this.top).height() - ( 160 + heads + tails + timeline ) );
+				}
+			});
 		},
 
 		//--------
